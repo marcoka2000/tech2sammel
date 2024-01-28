c837b6fa780502586534a7c407cc9eabb1e075d4
Marco Kade <marco.kade@ts.berlin>
2024-01-28 16:25:43 +0100

    added URLs for testing

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ const arrContentURLs = [
     "https://a6f67157-79f8-4152-b67c-51eeb92fc04e-00-193ix5fsxoh3s.kirk.replit.dev",
     "https://abf7e648-0da5-4c41-8944-7737c0f4e732-00-1eu7vl3jvdapx.riker.replit.dev",
     "https://ca762503-13b0-4c82-bcb8-af20181f8cd1-00-efngq341tcw3.worf.replit.dev",
-    "https://a380ee93-9c69-4893-ad24-b0e9c9894a1b-00-3afvcqx2mi5jg.riker.replit.dev",
+    //"https://a380ee93-9c69-4893-ad24-b0e9c9894a1b-00-3afvcqx2mi5jg.riker.replit.dev",
+   //"https://fabc5235-bad1-45e8-802d-482deee91b2b-00-1t5kcrau5yt5w.picard.replit.dev",
+   // Laura 
+   "https://b089c73f-7a1a-4b3f-ac45-5480b06f5c4b-00-7imuf1t5pqjx.worf.replit.dev",
+   // Darius 
    "https://fabc5235-bad1-45e8-802d-482deee91b2b-00-1t5kcrau5yt5w.picard.replit.dev",
-   // Darius "https://fabc5235-bad1-45e8-802d-482deee91b2b-00-1t5kcrau5yt5w.picard.replit.dev",
     "impressum.html"
 ]
 
