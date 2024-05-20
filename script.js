eb631d0aa7c93165a64a64bc2c5241cd4464c001
Marco Kade <marco.kade@ts.berlin>
2024-05-20 20:26:45 +0200

    initial commit to this branch

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const arrContentURLs = [
-    "ueber.html",
-    "/data/baeume/index.html",
-    "/data/kinder/index.html",
-    "/data/psychosozial/index.html",
-    "/data/radfahrende/index.html",
-    "/data/coronafallzahlen/index.html",
-    "/data/apothekendichte/index.html",
-    "/data/gewalt/index.html",
-    "/data/brokenlifts/index.html",
-    "/data/kriminalitsanalyse/index.html",
-    "/data/sommerspass/index.html",
-    "/data/psychosozial/index.html",
-    "/data/hitzehotspots/index.html",
-    "/",
-    "impressum.html"
-]
+  "ueber.html",
+  "/data/baeume/index.html",
+  "/data/kinder/index.html",
+  "/data/psychosozial/index.html",
+  "/data/radfahrende/index.html",
+  "/data/coronafallzahlen/index.html",
+  "/data/apothekendichte/index.html",
+  "/data/gewalt/index.html",
+  "/data/brokenlifts/index.html",
+  "/data/kriminalitsanalyse/index.html",
+  "/data/sommerspass/index.html",
+  "/data/psychosozial/index.html",
+  "/data/hitzehotspots/index.html",
+  "/",
+  "impressum.html",
+];
 
-document.getElementById("space").innerHTML = "<iframe src='" + arrContentURLs[0] + "'></iframe>";
+function fetchContent(path) {
+    fetch(path)
+    .then(function (response) {
+        if (response.ok) {
+        return response.text();
+        }
+        throw response;
+    })
+    .then(function (text) {
+        console.log(text);
+        document.getElementById("space").innerHTML = text;
+    });
+}
 
+fetchContent("ueber.html");
+
+// document.getElementById("space").innerHTML = "<iframe src='" + arrContentURLs[0] + "'></iframe>";
 
 const selectElement = document.querySelector("#project-select");
 
@@ -25,23 +40,26 @@ selectElement.addEventListener("change", (event) => {
     console.log(event.target.value);
 
     if (event.target.value != "Trenner") {
-        document.getElementById("space").innerHTML = "<iframe src='" + arrContentURLs[event.target.value] + "'></iframe>";
+        console.log(arrContentURLs[event.target.value]);
+        let content = fetchContent(arrContentURLs[event.target.value])
 
-        var links = document.getElementsByTagName("a");
+//        document.getElementById("space").innerHTML = "<iframe src='" + arrContentURLs[event.target.value] + "'></iframe>";
+
+       var links = document.getElementsByTagName("a");
         console.log(links)
-    }
-    
+     }
+
 });
 
-var iframe = document.getElementsByTagName("iframe");
-console.log(iframe)
+// var iframe = document.getElementsByTagName("iframe");
+// console.log(iframe)
 
-window.addEventListener('DOMContentLoaded', function() {
-    console.log('All asset are loaded')
-    var links = document.getElementsByTagName("a");
-    console.log(links)
+// window.addEventListener('DOMContentLoaded', function() {
+//     console.log('All asset are loaded')
+//     var links = document.getElementsByTagName("a");
+//     console.log(links)
 
-    for (var i = 0; i < links.length; i++) {
-        links[i].target = "_blank";
-    }
-})
+//     for (var i = 0; i < links.length; i++) {
+//         links[i].target = "_blank";
+//     }
+// })
