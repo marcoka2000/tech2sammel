f921552ed5694e39e0a60c1f3644c31c3d76b466
Marco Kade <marco.kade@ts.berlin>
2024-05-23 14:07:27 +0200

    chores, cleaning up master script.js

--- script.js
+++ script.js
@@ -1,80 +1,17 @@
-function fetchContent(path) {
-    fetch(path)
-    .then(function (response) {
-        if (response.ok) {
-        return response.text();
-        }
-        throw response;
-    })
-    .then(function (text) {
-        //console.log(text);
-        document.getElementById("header").innerHTML = text;
-    });
-}
-
-const arrContentURLs = [
-    "ueber.html",
-    "/data/baeume/index.html",
-    "/data/kinder/index.html",
-    "/data/psychosozial/index.html",
-    "/data/radfahrende/index.html",
-    "/data/coronafallzahlen/index.html",
-    "/data/apothekendichte/index.html",
-    "/data/gewalt/index.html",
-    "/data/brokenlifts/index.html",
-    "/data/kriminalitsanalyse/index.html",
-    "/data/sommerspass/index.html",
-    "/data/psychosozial/index.html",
-    "/data/hitzehotspots/index.html",
-    "/",
-    "impressum.html"
-]
-
-
 var button = document.createElement('a');
+button.setAttribute('id', 'back-button');
 button.setAttribute('href','../../index.html')
-button.setAttribute('style','font-family: sans-serif; font-weight: 400; background-color: #eee; color:#111; padding: 8px 16px; margin: 80px 8px; border-radius: 0px; border: 1px solid #6F6F6E; background: #ECECED; box-shadow: 4px 4px 0px 0px #6F6F6E; text-decoration: none;');
+button.setAttribute('style','font-family: sans-serif; font-weight: 400; color: hsl(215, 92%, 36%); padding: 8px 16px; border-radius: 0px; border: 2px solid #0059d8; background: #fff; box-shadow: 4px 4px 0px 0px #0059d8; text-decoration: none; position: fixed; top: 24px; z-index: 9000;');
 
-// position: fixed; top: 8px; left: 8px; z-index: 9000;');
-button.innerHTML = '← Zurück zur Übersicht'
+button.text = '← Zurück zur Übersicht'
 
 var spacer = document.createElement('div');
 spacer.setAttribute('class', 'spacer');
-spacer.setAttribute('style', 'height: 80px');
+spacer.setAttribute('style', 'margin: 40px;');
 
-console.log(button)
-// document.body.insertBefore(spacer, document.body.firstChild);
+spacer.setAttribute('style', 'max-width: 1200px; height: 80px; position: sticky;');
+
 document.body.insertBefore(button, document.body.firstChild);
+document.body.insertBefore(spacer, document.body.firstChild);
 
-//fetchContent("header.html")
-
-const selectElement = document.querySelector("#project-select");
-console.log(selectElement)
-
-selectElement.addEventListener("change", (event) => {
-    console.log(event.target.value);
-
-    if (event.target.value != "Trenner") {
-        window.location.href = arrContentURLs[0]
-    }
-    
-});
-
-/*
-document.getElementById("space").innerHTML = "<iframe src='" + arrContentURLs[0] + "'></iframe>";
-
-
-
-var iframe = document.getElementsByTagName("iframe");
-console.log(iframe)
-
-window.addEventListener('DOMContentLoaded', function() {
-    console.log('All asset are loaded')
-    var links = document.getElementsByTagName("a");
-    console.log(links)
-
-    for (var i = 0; i < links.length; i++) {
-        links[i].target = "_blank";
-    }
-});
-*/
+document.getElementById('card').setAttribute('style', 'margin-top: 0;')
